54b1fb31840f3a7721af38ca5608ab9f12c66b42
Umar Farooq <umar.wzbd@gmail.com>
2023-02-02 16:02:52 +0500

    changed server ul

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
-export const SERVER_URL = 'https://ksh.dev'
+// export const SERVER_URL = 'https://ksh.dev'
 // export const SERVER_URL = 'http://127.0.0.1:8000'
+export const SERVER_URL = 'https://dvgznb.deta.dev/'
 export const FRONTEND_URL = 'https://mehdimoii-reactapp.vercel.app'
+// export const FRONTEND_URL = 'http://127.0.0.1:5173'
